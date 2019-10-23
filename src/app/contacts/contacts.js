686edc2cb2427269cda23a7dff8eda0fc2220230
sokomishalov <sokomishalov@mail.ru>
2019-10-24 00:33:14 +0300

    minor fixes

--- src/app/contacts/contacts.js
+++ src/app/contacts/contacts.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./contacts.css"
-import {Button, Card, Tooltip} from "antd"
+import {Avatar, Button, Card, Tooltip} from "antd"
 import {EM_PAGE, FB_PAGE, GH_PAGE, IG_PAGE, TG_PAGE, VK_PAGE} from "../../util/consts/consts"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faFacebookF, faGithub, faInstagram, faTelegram, faVk} from "@fortawesome/free-brands-svg-icons"
 import _ from "lodash"
-import {faEnvelope, faFilePdf} from "@fortawesome/free-solid-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 import {downloadResource, openPage} from "../../util/window/window";
 import CV from "../../images/cv.pdf"
 
@@ -13,43 +13,43 @@ const PIC_SIZE = 250;
 
 const SOCIALS = [
     {
-        icon: faFilePdf,
+        icon: <Avatar style={{backgroundColor: "rgba(43,43,43,0.67)"}}>CV</Avatar>,
         color: "rgba(43,43,43,0.67)",
         tip: "My CV",
         onClick: async () => await downloadResource(CV, "sokomishalov-cv.pdf"),
     },
     {
-        icon: faFacebookF,
+        icon: <FontAwesomeIcon icon={faFacebookF}/>,
         color: "#3b5998",
         tip: "My facebook account",
         onClick: () => openPage(FB_PAGE),
     },
     {
-        icon: faVk,
+        icon: <FontAwesomeIcon icon={faVk}/>,
         color: "#4c75a3",
         tip: "My vk account",
         onClick: () => openPage(VK_PAGE),
     },
     {
-        icon: faInstagram,
+        icon: <FontAwesomeIcon icon={faInstagram}/>,
         color: "radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)",
         tip: "My instagram account",
         onClick: () => openPage(IG_PAGE),
     },
     {
-        icon: faTelegram,
+        icon: <FontAwesomeIcon icon={faTelegram}/>,
         color: "#0088cc",
         tip: "My telegram contact",
         onClick: () => openPage(TG_PAGE),
     },
     {
-        icon: faGithub,
+        icon: <FontAwesomeIcon icon={faGithub}/>,
         color: "#333",
         tip: "My github account",
         onClick: () => openPage(GH_PAGE),
     },
     {
-        icon: faEnvelope,
+        icon: <FontAwesomeIcon icon={faEnvelope}/>,
         color: "rgba(43,43,43,0.67)",
         tip: "Send email",
         onClick: () => openPage(EM_PAGE),
@@ -79,7 +79,7 @@ const Contacts = () => (
                                 className="contacts-socials-button"
                                 style={{background: s["color"]}}
                                 onClick={s["onClick"]}>
-                            <FontAwesomeIcon icon={s["icon"]}/>
+                            {s["icon"]}
                         </Button>
                     </Tooltip>
                 ))}
