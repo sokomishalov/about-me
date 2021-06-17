346a752b0fd64761a1ca5531787b2fc672886fcf
sokomishalov <sokomishalov@mail.ru>
2021-06-17 12:02:09 +0300

    years old fix

--- src/util/date/date.js
+++ src/util/date/date.js
@@ -1,4 +1,4 @@
 import { DateTime } from "luxon";
 import { BIRTHDAY } from "../consts/consts";
 
-export const getMyAge = () => DateTime.fromFormat(BIRTHDAY, 'dd.MM.yyyy').diffNow('years').years;
+export const getMyAge = () => Math.trunc(Math.abs(DateTime.fromFormat(BIRTHDAY, 'dd.MM.yyyy').diffNow('years').years)).toString();
