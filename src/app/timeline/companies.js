f53b4f5785c3a2d0b62c322f8093980e0393ac51
Mikhael Sokolov <mikhael.sokolov@mynd.co>
2025-01-24 15:34:50 +0300

    Add roofstock

--- src/app/timeline/companies.js
+++ src/app/timeline/companies.js
@@ -2,6 +2,7 @@ import SberbankLogo from "../../images/sber.png"
 import VtbLogo from "../../images/vtb.png"
 import BmstuLogo from "../../images/bmstu.png"
 import MyndLogo from "../../images/mynd.png"
+import RoofstockLogo from "../../images/roofstock.png"
 
 export const COMPANIES = [
     {
@@ -13,30 +14,37 @@ export const COMPANIES = [
     },
     {
         name: "Sberbank technology",
-        position: "Software engineer",
+        position: "Junior Software Engineer",
         years: "2015 - 2017",
         logo: SberbankLogo,
-        companyLink: "https://sber-tech.com/"
+        companyLink: "https://sbertech.ru/"
     },
     {
         name: "Sberbank",
-        position: "Software engineer",
+        position: "Middle Software Engineer",
         years: "2017 - 2019",
         logo: SberbankLogo,
         companyLink: "https://www.sberbank.ru"
     },
     {
         name: "VTB",
-        position: "Software engineer, solution architect",
+        position: "Senior Software Engineer, Solution Architect",
         years: "2019 - 2022",
         logo: VtbLogo,
         companyLink: "https://www.vtb.ru/"
     },
     {
         name: "Mynd Management",
-        position: "Senior software engineer",
-        years: "2022 - till now",
+        position: "Senior Software Engineer",
+        years: "2022 - 2024",
         logo: MyndLogo,
         companyLink: "https://www.mynd.co/"
+    },
+    {
+        name: "Roofstock",
+        position: "Senior Software Engineer",
+        years: "2024 - till now",
+        logo: RoofstockLogo,
+        companyLink: "https://www.roofstock.com/"
     },
 ]
