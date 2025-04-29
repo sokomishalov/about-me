bddc51fc52723e2eb4dee8d2a1970b477194edb7
Mikhael Sokolov <mikhael.sokolov@mynd.co>
2025-04-29 12:47:08 +0400

    restore activity

--- src/app/timeline/companies.js
+++ src/app/timeline/companies.js
@@ -3,35 +3,22 @@ import VtbLogo from "../../images/vtb.png"
 import BmstuLogo from "../../images/bmstu.png"
 import MyndLogo from "../../images/mynd.png"
 import RoofstockLogo from "../../images/roofstock.png"
+import JustPlayLogo from "../../images/justplay.png"
 
 export const COMPANIES = [
     {
-        name: "Bauman Moscow State Technical University",
-        position: "Student",
-        years: "2013 - 2017",
-        logo: BmstuLogo,
-        companyLink: "http://www.bmstu.ru/"
+        name: "JustPlay",
+        position: "Senior Software Engineer",
+        years: "2025 - till now",
+        logo: JustPlayLogo,
+        companyLink: "https://justplayapps.com/"
     },
     {
-        name: "Sberbank technology",
-        position: "Junior Software Engineer",
-        years: "2015 - 2017",
-        logo: SberbankLogo,
-        companyLink: "https://sbertech.ru/"
-    },
-    {
-        name: "Sberbank",
-        position: "Middle Software Engineer",
-        years: "2017 - 2019",
-        logo: SberbankLogo,
-        companyLink: "https://sberbank.ru"
-    },
-    {
-        name: "VTB",
-        position: "Senior Software Engineer, Solution Architect",
-        years: "2019 - 2022",
-        logo: VtbLogo,
-        companyLink: "https://vtb.ru/"
+        name: "Roofstock",
+        position: "Senior Software Engineer",
+        years: "2024 - 2025",
+        logo: RoofstockLogo,
+        companyLink: "https://roofstock.com/"
     },
     {
         name: "Mynd Management",
@@ -41,10 +28,31 @@ export const COMPANIES = [
         companyLink: "https://mynd.co/"
     },
     {
-        name: "Roofstock",
-        position: "Senior Software Engineer",
-        years: "2024 - till now",
-        logo: RoofstockLogo,
-        companyLink: "https://roofstock.com/"
+        name: "VTB",
+        position: "Senior Software Engineer, Solution Architect",
+        years: "2019 - 2022",
+        logo: VtbLogo,
+        companyLink: "https://vtb.ru/"
+    },
+    {
+        name: "Sberbank",
+        position: "Middle Software Engineer",
+        years: "2017 - 2019",
+        logo: SberbankLogo,
+        companyLink: "https://sberbank.ru"
+    },
+    {
+        name: "Sberbank technology",
+        position: "Junior Software Engineer",
+        years: "2015 - 2017",
+        logo: SberbankLogo,
+        companyLink: "https://sbertech.ru/"
+    },
+    {
+        name: "Bauman Moscow State Technical University",
+        position: "Student",
+        years: "2013 - 2017",
+        logo: BmstuLogo,
+        companyLink: "http://www.bmstu.ru/"
     },
 ]
